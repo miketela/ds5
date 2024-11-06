fb7f63ec4c343e43e1dbad37a3420cf12695abd1
michael <mportela19900@gmail.com>
2024-11-06 12:21:22 -0500

    Change Doned

--- lab3_3.js
+++
@@ -1,5 +0,0 @@
-function esBisiesto(año) {
-    const resultado = (año % 4 === 0 && año % 100 !== 0) || (año % 400 === 0);
-    console.log(`El año ${año} es bisiesto: ${resultado}`);
-    return resultado;
-}
